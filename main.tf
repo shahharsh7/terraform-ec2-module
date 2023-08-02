53391274c4556564c1f3aa25e453cc96b6faca84
= <=>
2023-08-02 09:06:49 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.9.0"
+    }
+  }
+}
+provider "aws" {
+      region = var.aws_region
+}
+resource "aws_instance" "Nishant-Amazon-Linux-instance" {
+    count = 1
+    ami = "ami-0ded8326293d3201b"
+    instance_type = var.instance_type
+    key_name = "Nishant-Keypair"
+    user_data = file("apache.sh")
+    tags = {
+        Name = "Nishant machine no ${count.index +1}"
+    }
+}
